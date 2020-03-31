658709da978262930dfb1bd1fd5340031e2e91b8
Laurent Harel <lrt.hrl@gmail.com>
2020-03-31 12:04:55 -0400

    chore(packages): update config
    affects: @lornz-/app, @lornz-/common

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
-  const commonFunction = require("@Lornz-/common");
+  const commonFunction = require("@lornz-/common");
 
   commonFunction();
 
